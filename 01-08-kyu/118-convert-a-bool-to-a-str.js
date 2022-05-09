c2afe318739eb97b0baf509fc01298ae0568944c
faraaiden <faraaiden.contact@gmail.com>
2022-05-09 23:26:28 +0700

    Add solution

---
+++ 01-08-kyu/118-convert-a-bool-to-a-str.js
@@ -0,0 +1,28 @@
+/*
+============
+INSTRUCTIONS
+============
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+const booleanToString = b => String(b);
+
+
+/*
+=====================
+ALTERNATIVE SOLUTIONS
+=====================
+
+const booleanToString = b => b.toString();
+
+---------------------
+
+const booleanToString = String;
+
+---------------------
+
+const booleanToString = b => `${b}`;
+
+*/
