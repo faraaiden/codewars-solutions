3bfe0fa56f4f28796dd0ec64833ad5b594c8621c
faraaiden <faraaiden.contact@gmail.com>
2022-05-09 08:39:18 +0700

    Add solution

---
+++ 01-08-kyu/108-convert-a-number-to-a-str.js
@@ -0,0 +1,30 @@
+/*
+============
+INSTRUCTIONS
+============
+We need a function that can transform a number into a string.
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+*/
+
+const numberToString = num => String(num);
+
+/*
+=====================
+ALTERNATIVE SOLUTIONS
+=====================
+
+const numberToString = num => num.toString();
+
+---------------------
+
+const numberToString = num => num + '';
+
+---------------------
+
+const numberToString = String;
+
+*/
