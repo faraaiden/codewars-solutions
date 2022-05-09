3b926ad8f054f7b25083fc8d39d2894a746fa3a3
faraaiden <faraaiden.contact@gmail.com>
2022-05-10 01:01:23 +0700

    Fix the previous solution

---
+++ 01-08-kyu/130-sum-mixed-array.js
@@ -0,0 +1,10 @@
+/*
+============
+INSTRUCTIONS
+============
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+const sumMix = x => x.reduce((acc, curr) => acc + +curr, 0);
