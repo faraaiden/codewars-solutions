d6178980a36ff0941d497c78028bbf5420a5074b
faraaiden <faraaiden.contact@gmail.com>
2022-05-13 12:59:38 +0700

    Add solution

---
+++ 01-08-kyu/169-squaring-an-arg.js
@@ -0,0 +1,21 @@
+/*
+============
+INSTRUCTIONS
+============
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+const square = num => Math.pow(num, 2);
+
+/*
+====================
+ALTERNATIVE SOLUTION
+====================
+
+const square = num => num ** 2;
+
+--------------------
+
+const square = num => num * num;
+
+*/
