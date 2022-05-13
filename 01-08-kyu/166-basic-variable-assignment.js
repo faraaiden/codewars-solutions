31d6073f0a862e88bdb4686ff68e1e7998eac5c9
faraaiden <faraaiden.contact@gmail.com>
2022-05-13 12:18:25 +0700

    Add solution

---
+++ 01-08-kyu/166-basic-variable-assignment.js
@@ -0,0 +1,14 @@
+/*
+============
+INSTRUCTIONS
+============
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+*/
+
+const a = 'code';
+const b = 'wa.rs';
+const name = a + b;
