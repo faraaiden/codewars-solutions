28466643583eb5c01e9fce8cb5450f5b79ae6593
faraaiden <faraaiden.contact@gmail.com>
2022-05-14 20:09:20 +0700

    Add solution

---
+++ 01-08-kyu/180-grasshopper-terminal-game-combat-function.js
@@ -0,0 +1,8 @@
+/*
+============
+INSTRUCTIONS
+============
+Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
+*/
+
+const combat = (health, damage) => health > damage ? health - damage : 0;
