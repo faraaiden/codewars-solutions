19750a14129509bc9554c1dd3c66b6e4b2b51fff
faraaiden <105180919+faraaiden@users.noreply.github.com>
2022-05-24 20:30:17 +0700

    Add solution

---
+++ 01-08-kyu/214-grasshopper-variable-assignment-debug.js
@@ -0,0 +1,16 @@
+/*
+============
+INSTRUCTIONS
+============
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+var a == "dev"
+var b == "Lab"
+
+var name == a + b
+*/
+
+const a = "dev";
+const b = "Lab";
+
+const name = a + b;
